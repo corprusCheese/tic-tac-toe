62d2c04876bc9c9d29226a702e546cc7582bc424
corprusCheese <samson20100@yandex.ru>
2021-06-29 16:18:31 +0300

    host 0.0.0.0

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,7 +9,7 @@ object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
     BlazeServerBuilder[IO]
-      .bindHttp(8080, "localhost")
+      .bindHttp(8080, "0.0.0.0")
       .withHttpApp(HttpService.gameService)
       .serve
       .compile
