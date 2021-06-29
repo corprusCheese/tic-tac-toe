a5791b70b8b1b2bb768e5bc1f112ba9cc0b82570
corprusCheese <samson20100@yandex.ru>
2021-06-29 14:57:56 +0300

    initial frontend

---
+++ src/main/resources/frontend/js/index.js
@@ -0,0 +1,41 @@
+const ceilSize = 100
+
+function getBoard(dim) {
+    let html = ""
+    for(let i=0; i<dim; i++) {
+        html+=getRow(dim)
+    }
+    return boardWrapper(html, dim)
+}
+
+function boardWrapper(html, dim) {
+    let width = dim*ceilSize + 10 // + borders
+    return "<div style='width: "+width+"px; margin: auto; margin-top: 5%'>" + html + "</div>"
+}
+
+function getRow(dim) {
+    let html = ""
+    for (let i=0; i<dim; i++) {
+        html+=getCeil()
+    }
+    return html
+}
+
+function getCeil() {
+    return "<div class='ceil' style='width: "+ceilSize+"px; height: "+ceilSize+"px'></div>"
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementsByClassName("board")[0].outerHTML = getBoard(3)
+
+    let cells = document.getElementsByClassName("ceil");
+
+    function sendQuery() {
+        console.log("ceil clicked")
+    }
+
+    for (let i=0; i<cells.length; i++) {
+        cells[i].addEventListener("click", sendQuery, false)
+    }
+
+})
