d4674a94bc2902d6867cebee042c4fa51ee73257
corprusCheese <samson20100@yandex.ru>
2021-07-13 13:46:13 +0300

    question-1

--- src/main/scala/HttpService/WebServices/ChatService.scala
+++ src/main/scala/HttpService/WebServices/ChatService.scala
@@ -3,7 +3,6 @@ package HttpService.WebServices
 
 import helloworld.HttpService.Algebra.AbstractService
 import helloworld.HttpService.Game.Logic._
-
 import cats.effect.IO
 import cats.effect.concurrent._
 import cats.implicits._
@@ -14,35 +13,103 @@ import org.http4s.dsl.Http4sDsl
 import org.http4s.server.websocket.WebSocketBuilder
 import org.http4s.websocket.WebSocketFrame
 import org.http4s.websocket.WebSocketFrame.Text
+import cats._
+import cats.data.Ior
+import cats.effect.{Concurrent, Timer}
+import cats.implicits._
 
 import scala.collection.mutable
 import scala.language.implicitConversions
+import cats.data.Ior
+import helloworld.HttpService.WebServices.ChatService.IorUserList
 
 
-class ChatService(consumersList: Ref[IO, List[Queue[IO, WebSocketFrame]]]) extends Http4sDsl[IO] with AbstractService {
+class ChatService(consumersListOfIors: Ref[IO, IorUserList]) extends Http4sDsl[IO] with AbstractService {
 
   private val chatService: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case req@GET -> Root / "start" =>
-          val inStreamProcessor2: Pipe[IO, WebSocketFrame, Unit] = stream => {
-            stream.mapAccumulate("")({
-              case (name, frame@Text(str, _)) if name == "" =>
-                (str, none[String])
-              case (name, frame@Text(str, _)) =>
-                (name, str.some)
-            }).collect({
-              case (name, Some(message)) => (name, message)
-            }).evalMap({
-              case (name, message) =>
-                consumersList.get.flatMap(_.map(_.enqueue1(Text(s"$name: $message"))).sequence).map(_ => ())
-            })
-          }
+      // second
+      val inStreamProcessor2: Pipe[IO, WebSocketFrame, Unit] = stream => {
 
-          val outStream: Stream[IO, WebSocketFrame] = {
-            Stream.eval(Queue.unbounded[IO, WebSocketFrame])
-              .evalMap(queue => consumersList.update(queue :: _).map(_ => queue))
-              .flatMap(queue => queue.dequeue)
-          }
-          WebSocketBuilder[IO].build(outStream, inStreamProcessor2)
+        println(3)
+        /*consumersListOfIors.get.map(iorUserList => {
+          iorUserList.map(ior => {
+            val newIor = ior.left match {
+              case None =>
+                ior.putLeft(stream)
+                ior
+              case _ => ior
+            }
+            newIor
+          })
+          println(iorUserList)
+          iorUserList
+        })*/
+        val ior: Ior.Left[Stream[IO, WebSocketFrame]] = Ior.Left(stream)
+
+        consumersListOfIors.update(ior::_)
+
+        consumersListOfIors.get.map(iorUserList => {
+          iorUserList.map(ior => {
+            println(ior)
+            ior
+          })
+          iorUserList
+        })
+
+        println(ior)
+        println(consumersListOfIors.get)
+
+        println(4)
+
+        stream.mapAccumulate("")({
+          case (name, frame@Text(str, _)) if name == "" =>
+            (str, none[String])
+          case (name, frame@Text(str, _)) =>
+            (name, str.some)
+        }).collect({
+          case (name, Some(message)) => (name, message)
+        }).evalMap({
+          case (name, message) =>
+            consumersListOfIors
+              .get
+              .flatMap(_.map(ior => {
+                println(ior.left.get)
+                println(stream)
+                println(ior.left.get != stream)
+                if (ior.left.get != stream) {
+                  ior.right.get.enqueue1(Text(s"$name: $message"))
+                } else {
+                  ior.right.get.enqueue1(Text(s"You send the message!"))
+                }
+              }).sequence).map(_ => ())
+        })
+      }
+
+      // 1 3 4 5 2
+      val outStream: Stream[IO, WebSocketFrame] = {
+        println(1)
+        Stream.eval(Queue.unbounded[IO, WebSocketFrame])
+          .evalMap(queue => {
+            println(5)
+            consumersListOfIors.update(iorUserList => {
+              iorUserList.map(ior => {
+                println(ior)
+                val newIor = ior.right match {
+                  case None =>
+                    ior.putRight(queue)
+                  case _ => ior
+                }
+                newIor
+              })
+            }).map(_=>queue)
+            //consumersListOfIors.update(ior::_).map(_ => queue)
+          }).flatMap(queue => {
+          println(2)
+          queue.dequeue
+        })
+      }
+      WebSocketBuilder[IO].build(outStream, inStreamProcessor2)
   }
 
   override def getInstance(): HttpRoutes[IO] = chatService
@@ -50,14 +117,9 @@ class ChatService(consumersList: Ref[IO, List[Queue[IO, WebSocketFrame]]]) exten
 
 object ChatService {
 
-  type Name = String
-  case class ChatMessage(name: Name, wsf: WebSocketFrame) {
-    def get(): String = {
-      s"$name: $wsf.data"
-    }
-  }
+  type IorUserList = List[Ior[Stream[IO, WebSocketFrame], Queue[IO, WebSocketFrame]]]
 
   def apply(): IO[ChatService] = for {
-    consumers <- Ref.of[IO, List[Queue[IO, WebSocketFrame]]](List.empty)
+    consumers <- Ref.of[IO, IorUserList](List.empty)
   } yield new ChatService(consumers)
 }
